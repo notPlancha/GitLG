85aed9638145a9477df87ef5e325478d9a3ab1d6
notPlancha <31565873+notPlancha@users.noreply.github.com>
2025-05-03 17:51:58 +0200

    changed to bun

--- web/src/components.d.ts
+++ web/src/components.d.ts
@@ -5,43 +5,43 @@
 	// https://stackoverflow.com/a/70980761/3779853
 
 	import { RecycleScroller } from 'vue-virtual-scroller'
-	import App from './App.vue'
-	import AllBranches from './views/AllBranches.vue'
-	import CommitDetails from './views/CommitDetails.vue'
-	import CommitFileChanges from './views/CommitFileChanges.vue'
-	import CommitRefTips from './views/CommitRefTips.vue'
-	import CommitRow from './views/CommitRow.vue'
-	import CommitsDetails from './views/CommitsDetails.vue'
-	import GitActionButton from './views/GitActionButton.vue'
-	import GitInput from './views/GitInput.vue'
-	import History from './views/History.vue'
-	import MainView from './views/MainView.vue'
-	import RefTip from './views/RefTip.vue'
-	import RepoSelection from './views/RepoSelection.vue'
-	import SVGVisualization from './views/SVGVisualization.vue'
-	import SelectedGitAction from './views/SelectedGitAction.vue'
-	import Popup from './components/Popup.vue'
-	import PromiseForm from './components/PromiseForm.vue'
+	import src\App from '.\App.vue'
+	import src\views\AllBranches from '.\views\AllBranches.vue'
+	import src\views\CommitDetails from '.\views\CommitDetails.vue'
+	import src\views\CommitFileChanges from '.\views\CommitFileChanges.vue'
+	import src\views\CommitRefTips from '.\views\CommitRefTips.vue'
+	import src\views\CommitRow from '.\views\CommitRow.vue'
+	import src\views\CommitsDetails from '.\views\CommitsDetails.vue'
+	import src\views\GitActionButton from '.\views\GitActionButton.vue'
+	import src\views\GitInput from '.\views\GitInput.vue'
+	import src\views\History from '.\views\History.vue'
+	import src\views\MainView from '.\views\MainView.vue'
+	import src\views\RefTip from '.\views\RefTip.vue'
+	import src\views\RepoSelection from '.\views\RepoSelection.vue'
+	import src\views\SelectedGitAction from '.\views\SelectedGitAction.vue'
+	import src\views\SVGVisualization from '.\views\SVGVisualization.vue'
+	import src\components\Popup from '.\components\Popup.vue'
+	import src\components\PromiseForm from '.\components\PromiseForm.vue'
 
 	declare module '@vue/runtime-core' {
 		export interface GlobalComponents {
 			RecycleScroller: typeof RecycleScroller
-			App: typeof App
-			AllBranches: typeof AllBranches
-			CommitDetails: typeof CommitDetails
-			CommitFileChanges: typeof CommitFileChanges
-			CommitRefTips: typeof CommitRefTips
-			CommitRow: typeof CommitRow
-			CommitsDetails: typeof CommitsDetails
-			GitActionButton: typeof GitActionButton
-			GitInput: typeof GitInput
-			History: typeof History
-			MainView: typeof MainView
-			RefTip: typeof RefTip
-			RepoSelection: typeof RepoSelection
-			SVGVisualization: typeof SVGVisualization
-			SelectedGitAction: typeof SelectedGitAction
-			Popup: typeof Popup
-			PromiseForm: typeof PromiseForm
+			src\App: typeof src\App
+			src\views\AllBranches: typeof src\views\AllBranches
+			src\views\CommitDetails: typeof src\views\CommitDetails
+			src\views\CommitFileChanges: typeof src\views\CommitFileChanges
+			src\views\CommitRefTips: typeof src\views\CommitRefTips
+			src\views\CommitRow: typeof src\views\CommitRow
+			src\views\CommitsDetails: typeof src\views\CommitsDetails
+			src\views\GitActionButton: typeof src\views\GitActionButton
+			src\views\GitInput: typeof src\views\GitInput
+			src\views\History: typeof src\views\History
+			src\views\MainView: typeof src\views\MainView
+			src\views\RefTip: typeof src\views\RefTip
+			src\views\RepoSelection: typeof src\views\RepoSelection
+			src\views\SelectedGitAction: typeof src\views\SelectedGitAction
+			src\views\SVGVisualization: typeof src\views\SVGVisualization
+			src\components\Popup: typeof src\components\Popup
+			src\components\PromiseForm: typeof src\components\PromiseForm
 		}
 	}
